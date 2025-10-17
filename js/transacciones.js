262dcfcf404178460f276190fdf02bddf48c365d
Gustavo Fernandez <gustavofernandbus@gmail.com>
2025-10-17 01:44:55 -0300

    js.1.1

--- js/transacciones.js
+++ js/transacciones.js
@@ -1,21 +1,13 @@
-// Obtener elementos
-const boton = document.getElementById('btn-depositar');
-const modal = document.getElementById('miModal');
-const cerrar = document.querySelector('.cerrar');
+let boton = document.getElementById('btn-depositar');
+let modal = document.getElementById('miModal');
+let cerrar = document.querySelector('.cerrar');
 
-// Abrir modal al hacer clic en el botón
 boton.addEventListener('click', function() {
   modal.style.display = 'block';
 });
 
-// Cerrar modal al hacer clic en la X
 cerrar.addEventListener('click', function() {
   modal.style.display = 'none';
 });
 
-// Cerrar modal al hacer clic fuera del contenido
-window.addEventListener('click', function(event) {
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
-});
+
